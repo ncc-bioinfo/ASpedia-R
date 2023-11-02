38fd2240159680492aca49dc9ee3e1cc1dca24ae
ncc bioinfo <ncc.bioinfo@gmail.com>
2023-11-02 16:58:37 +0900

    commit

--- R/asr_plot.R
+++ R/asr_plot.R
@@ -533,12 +533,10 @@ asr_plot <- function(annotation.result="", gtf.file.name="", gene.model="Ensembl
     }
   
     #gene plot
-    print(class(gene.data))
     gene.plot.data <- GenomicRanges::split(gene.data, gene.data$transcript_id)
-    print(gene.plot.data)
     gene.text.position <- mean(exon.region)
-    gene.plot <- autoplot(object=gene.plot.data, label.color="black", aes(fill=exon_type)) + scale_fill_manual(values=c("exon"="#619CFF", "as_exon"="#F8766D")) + ylab(paste0("Gene\n(", gene.id, ",", gene.name, ")")) + xlim(exon.region) + guides(y="none")
-    gene.plot@ggplot$layers[[4]]$data$midpoint <- gene.text.position
+    gene.plot <- autoplot(gene.plot.data, label.color="black", aes(fill=exon_type)) + scale_fill_manual(values=c("exon"="#619CFF", "as_exon"="#F8766D")) + ylab(paste0("Gene\n(", gene.id, ",", gene.name, ")")) + xlim(exon.region) + guides(y="none")
+    gene.plot@ggplot$layers[[length(gene.plot@ggplot$layers)]]$data$midpoint <- gene.text.position
   
     #as.track <- tracks(ideo.plot, gene.plot)
     as.track <- tracks(gene.plot)
