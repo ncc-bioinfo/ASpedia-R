00ba4043e148816f3ad426182d300e7e430d8e89
ncc bioinfo <ncc.bioinfo@gmail.com>
2023-11-03 11:22:26 +0900

    commit

--- R/asr_plot.R
+++ R/asr_plot.R
@@ -171,10 +171,14 @@ asr_plot <- function(annotation.result="", gtf.file.name="", gene.model="Ensembl
   }
   
   #check heights list
-  if((class(plot.data.list) == "character" && plot.data.list[1] != "") && (class(heigths.list) == "character" && heigths.list[1] != "")) {
+  has.heigths.list < FALSE
+  
+  if((class(plot.data.list) == "character" && plot.data.list[1] != "") && (class(heigths.list) == "numeric")) {
     if((length(plot.data.list) + 2) != length(heights.list)) {
-      print("*** ERROR MESSAGE: Input heights list is wrong. Please check your input heights list.")
+      print("*** ERROR MESSAGE: Input plot data list and heights list is not matched. Please check your input heights list or heights list.")
       return()
+    } else {
+      has.heights.list <- TRUE
     }
   }
   
@@ -826,7 +830,7 @@ asr_plot <- function(annotation.result="", gtf.file.name="", gene.model="Ensembl
       repeats.text.data$x <- as.numeric(repeats.text.data$x)
       repeats.text.data$y <- as.numeric(repeats.text.data$y)
         
-      repeats.plot <- ggplot() + geom_rect(data=repeats.data, mapping=aes(xmin=xmin, xmax=xmax, ymin=ymin, ymax=ymax, fill=I("pink"))) + geom_text(data=repeats.text.data, mapping=aes(x=x, y=y, label=repeats_name), hjust=0, vjust=0.5) + ylab("Repeats") + guides(y="none") + xlim(exon.region) + ylim(0, (max(repeats.data$ymax) + 0.5))
+      repeats.plot <- ggplot() + geom_rect(data=repeats.data, mapping=aes(xmin=xmin, xmax=xmax, ymin=ymin, ymax=ymax, fill=I("pink"))) + geom_text(data=repeats.text.data, mapping=aes(x=x, y=y, label=repeats_name), hjust=0, vjust=0) + ylab("Repeats") + guides(y="none") + xlim(exon.region) + ylim(0, (max(repeats.data$ymax) + 0.5))
       repeats.track <- tracks(repeats.plot)
         
       prev.track.height <- as.track@heights
@@ -854,7 +858,7 @@ asr_plot <- function(annotation.result="", gtf.file.name="", gene.model="Ensembl
         target.index <- target.index + 1
       }
       
-      rbp.plot <- ggplot(rbp.plot.data, aes(x=peak_start, y=ymin, name=target)) + geom_segment(aes(xend=peak_end, yend=ymin, color=target), linewidth=4) + ylab("RNA binding protein") + guides(y="none") + xlim(exon.region) + ylim(0, max(rbp.plot.data$ymax) + 1)
+      rbp.plot <- ggplot(rbp.plot.data, aes(x=peak_start, y=ymax, name=target)) + geom_segment(aes(xend=peak_end, yend=ymax, color=target), linewidth=4) + ylab("RNA binding protein") + guides(y="none") + xlim(exon.region) + ylim(0, max(rbp.plot.data$ymax) + 0.5)
       rbp.plot.build <- ggplot_build(rbp.plot)
       rbp.plot.raw.data <- rbp.plot.build$data[[1]]
       
@@ -885,7 +889,7 @@ asr_plot <- function(annotation.result="", gtf.file.name="", gene.model="Ensembl
         legend.element.index <- legend.element.index + 1
       }
       
-      rbp.legend.plot <- ggplot(rbp.legend.data, aes(x=x, y=y)) + geom_segment(aes(xend=xend, yend=y, color=legend_color), linewidth=4) + geom_text(aes(x=xend, label=target, color=legend_color), size=3, vjust=0.5, hjust=-0.1) + guides(y="none") + xlim(exon.region) + ylim(0, max(rbp.legend.data$y) + 1)
+      rbp.legend.plot <- ggplot(rbp.legend.data, aes(x=x, y=y)) + geom_segment(aes(xend=xend, yend=y, color=legend_color), linewidth=4) + geom_text(aes(x=xend, label=target, color=legend_color), size=3, vjust=0.5, hjust=-0.1) + guides(y="none") + ylab("") + xlim(exon.region) + ylim(0, max(rbp.legend.data$y) + 1)
       
       rbp.track <- tracks(rbp.plot)
       rbp.legend.track <- tracks(rbp.legend.plot)
@@ -907,7 +911,7 @@ asr_plot <- function(annotation.result="", gtf.file.name="", gene.model="Ensembl
     
     as.track <- as.track + theme_tracks_sunset(bg="white") + theme(legend.position="none", axis.title.y=element_text(angle=0, vjust=0.5))
     
-    if(heights.list != "") {
+    if(has.heights.list == TRUE) {
       as.track@heights <- heights.list
     }
     
